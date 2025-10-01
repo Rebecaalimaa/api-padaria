eb8d2d8b93e1bd588fbdf5a7478505734dc486da
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-10-01 10:36:50 -0300

    ok

--- src/router.js
+++ src/router.js
@@ -68,39 +68,39 @@ routes.patch('/clientes/:id',  Cliente.update);
 routes.delete('/clientes/:id', Cliente.remove);
 
 routes.get('/produtos', Produto.read);
-routes.post('/produtos', MiddlewareAuth.validate, Produto.create);
-routes.patch('/produtos/:id', MiddlewareAuth.validate, Produto.update);
-routes.delete('/produtos/:id', MiddlewareAuth.validate, Produto.remove);
+routes.post('/produtos',  Produto.create);
+routes.patch('/produtos/:id',  Produto.update);
+routes.delete('/produtos/:id',  Produto.remove);
 
-routes.get('/pedidos', MiddlewareAuth.validate, Pedido.read);
-routes.get('/pedidos/:id', MiddlewareAuth.validate, Pedido.readOne);
-routes.post('/pedidos', MiddlewareAuth.validate, Pedido.create);
-routes.patch('/pedidos/:id', MiddlewareAuth.validate, Pedido.update);
-routes.delete('/pedidos/:id', MiddlewareAuth.validate, Pedido.remove);
+routes.get('/pedidos',  Pedido.read);
+routes.get('/pedidos/:id',  Pedido.readOne);
+routes.post('/pedidos',  Pedido.create);
+routes.patch('/pedidos/:id',  Pedido.update);
+routes.delete('/pedidos/:id',  Pedido.remove);
 
-routes.get('/item', MiddlewareAuth.validate, Item.read);
-routes.get('/item/:id', MiddlewareAuth.validate, Item.readOne);
-routes.post('/item', MiddlewareAuth.validate, Item.create);
-routes.patch('/item/:id', MiddlewareAuth.validate, Item.update);
-routes.delete('/item/:id', MiddlewareAuth.validate, Item.remove);
+routes.get('/item',  Item.read);
+routes.get('/item/:id',  Item.readOne);
+routes.post('/item',  Item.create);
+routes.patch('/item/:id',  Item.update);
+routes.delete('/item/:id',  Item.remove);
 
-routes.get('/funcionarios', MiddlewareAuth.validate, Funcionario.read);
-routes.get('/funcionarios/:id', MiddlewareAuth.validate, Funcionario.readOne);
-routes.post('/funcionarios', MiddlewareAuth.validate, Funcionario.create);
-routes.patch('/funcionarios/:id', MiddlewareAuth.validate, Funcionario.update);
-routes.delete('/funcionarios/:id', MiddlewareAuth.validate, Funcionario.remove);
+routes.get('/funcionarios',  Funcionario.read);
+routes.get('/funcionarios/:id',  Funcionario.readOne);
+routes.post('/funcionarios',  Funcionario.create);
+routes.patch('/funcionarios/:id',  Funcionario.update);
+routes.delete('/funcionarios/:id',  Funcionario.remove);
 
-routes.get('/estoque', MiddlewareAuth.validate, Estoque.read);
-routes.get('/estoque/:id', MiddlewareAuth.validate, Estoque.readOne);
-routes.post('/estoque', MiddlewareAuth.validate, Estoque.create);
-routes.patch('/estoque/:id', MiddlewareAuth.validate, Estoque.update);
-routes.delete('/estoque/:id', MiddlewareAuth.validate, Estoque.remove);
+routes.get('/estoque',  Estoque.read);
+routes.get('/estoque/:id',  Estoque.readOne);
+routes.post('/estoque',  Estoque.create);
+routes.patch('/estoque/:id',  Estoque.update);
+routes.delete('/estoque/:id',  Estoque.remove);
 
-routes.get('/fornecedores', MiddlewareAuth.validate, Fornecedor.read);
-routes.get('/fornecedores/:id', MiddlewareAuth.validate, Fornecedor.readOne);
-routes.post('/fornecedores', MiddlewareAuth.validate, Fornecedor.create);
-routes.patch('/fornecedores/:id', MiddlewareAuth.validate, Fornecedor.update);
-routes.delete('/fornecedores/:id', MiddlewareAuth.validate, Fornecedor.remove);
+routes.get('/fornecedores',  Fornecedor.read);
+routes.get('/fornecedores/:id',  Fornecedor.readOne);
+routes.post('/fornecedores',  Fornecedor.create);
+routes.patch('/fornecedores/:id',  Fornecedor.update);
+routes.delete('/fornecedores/:id',  Fornecedor.remove);
 
 
 
