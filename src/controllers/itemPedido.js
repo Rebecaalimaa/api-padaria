c285f062bd7008e712db233e6fa12156ebb2bf30
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-10-07 14:36:03 -0300

    ok

--- src/controllers/itemPedido.js
+++ src/controllers/itemPedido.js
@@ -2,39 +2,40 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const create = async (req, res) => {
-    req.body.sub_total = req.body.pizza.valor * req.body.quantidade;
     try {
-        const item = await prisma.item.create({
+        req.body.valor_total = req.body.preco_unitario * req.body.quantidade;
+
+        const item = await prisma.item_Pedido.create({
             data: req.body
         });
         return res.status(201).json(item);
     } catch (error) {
+        console.error(error);
         return res.status(400).json({ error: error.message });
     }
-}
+};
 
 const read = async (req, res) => {
-    const item = await prisma.item.findMany();
+    const item = await prisma.item_Pedido.findMany();
     return res.json(item);
-}
+};
 
 const readOne = async (req, res) => {
     try {
-        const item = await prisma.item.findUnique({
+        const item = await prisma.item_Pedido.findUnique({
             where: {
                 item_id: Number(req.params.id)
-
             }
         });
         return res.json(item);
     } catch (error) {
         return res.status(400).json({ error: error.message });
     }
-}
+};
 
 const update = async (req, res) => {
     try {
-        const item = await prisma.item.update({
+        const item = await prisma.item_Pedido.update({
             where: {
                 item_id: Number(req.params.id)
             },
@@ -44,25 +45,25 @@ const update = async (req, res) => {
     } catch (error) {
         return res.status(400).json({ error: error.message });
     }
-}
+};
 
 const remove = async (req, res) => {
     try {
-        await prisma.item.delete({
+        await prisma.item_Pedido.delete({
             where: {
-                item_id : Number(req.params.id)
+                item_id: Number(req.params.id)
             }
         });
         return res.status(204).send();
     } catch (error) {
         return res.status(404).json({ error: error.message });
     }
-}
+};
 
-module.exports = { 
+module.exports = {
     create,
-    read, 
-    readOne, 
-    update, 
-    remove 
+    read,
+    readOne,
+    update,
+    remove
 };
