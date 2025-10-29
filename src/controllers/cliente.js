61118f76eff48160738a5fe926c65178f500a788
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-10-29 09:49:57 -0300

    .

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 const { createHash } = require('../middlewares/auth'); 
 const create = async (req, res) => {
     try {
