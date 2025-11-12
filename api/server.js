faa77aa301e4b68594f2d38adb45db59afd5bfb3
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-11-12 16:51:06 -0300

    ok

--- api/server.js
+++ api/server.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-/*const swaggerUi = require('swagger-ui-express');*/
-
-/*const swaggerDoc = require('../swagger.json');*/
 const routes = require('../src/router');
 const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-/*app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));*/
 
 app.use(routes);
 
